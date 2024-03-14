feaf76c35d0f73c5a3610c23458287cdfcf3a694
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2024-03-14 18:05:00 +0530

    practice hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useMutation, useQuery } from "react-query";
 
 const App = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["todo"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3500/todos");
+      if (response.ok) {
+        // console.log(response.data);
+      }
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
 
-export default App
+  const { mutate } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("http://localhost:3500/todos", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+      }).then((res) => res.json()),
+  });
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>data is loading</div>;
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          mutate({
+            userId: 400,
+            id: "300",
+            title: "Buy Toys",
+            completed: false,
+          })
+        }
+      >
+        Add Post
+      </button>
+      {data?.map((todo) => (
+        <div>
+          <h3>ID: {todo.id}</h3>
+          <h4>TITLE: {todo.title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
